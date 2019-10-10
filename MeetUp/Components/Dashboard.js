b28b197d0e064abec69c278d03892bf18f8f048b
Sami <BlueBubbleBeam@outlook.com>
2019-10-09 22:59:15 -0400

    styled greeting and profile settings components

--- MeetUp/Components/Dashboard.js
+++ MeetUp/Components/Dashboard.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 import {createDrawerNavigator, DrawerNavigatorItems} from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
-import {Button,Text, View, SafeAreaView, ScrollView} from 'react-native';
-import Main from './Main';
+import {TouchableOpacity, Button,Text, View, SafeAreaView, ScrollView, StyleSheet, Dimensions, Image } from 'react-native';
+import Main from './Main'; 
 import { TextInput } from 'react-native-gesture-handler';
+
+const vh = Dimensions.get("window").height;
+const vw = Dimensions.get("window").width;
 
 class Map extends Component{
   static navigationOptions = {
@@ -12,36 +15,95 @@ class Map extends Component{
   render() {
     return (
       <View>
-        <Text>MAP HERE</Text>
+        <Text>MAP HERE</Text> 
       </View>
     );
   }
 }
-
 
 class Greeting extends Component {    //each dashboard component gets its own navigation options config
     static navigationOptions = {
     };
 
     render() {
+      const style = styles.Greeting;
       return (
-        <View>
-          <Text>Welcome to MeetUp User</Text>
+        <View style = {style.container}>
+          <Text style={style.text}>Welcome Back,</Text>
+          <Text style={style.text}>User</Text>
         </View>
       );
     }
 }
 
+const styles = StyleSheet.create({
+  Greeting:{
+    container:{
+      borderWidth: 2,
+      borderColor: '#F0F0F0',
+      marginLeft: .05*vw,
+      marginRight: .05*vw,
+      paddingTop: .05*vh,
+      paddingBottom: .05*vh,
+      borderStyle: 'dashed',
+      borderRadius: 1
+    },
+    text:{
+      fontSize: 24,
+      textAlign: 'center',
+      color: 'gold',
+    }
+  },
+  ProfileAndSettings:{
+    container:{
+      paddingTop: .005*vh,
+      paddingBottom: .005*vh,
+      borderTopWidth: 2,
+      borderBottomWidth: 2,
+      borderColor: 'black',
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    },
+    tap:{
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center'
+    },
+    profile:{
+      height: 42,
+      width: 42,
+      flex: 1
+    },
+    gear:{
+      height: 38,
+      width: 38,
+      flex: 1
+    },
+    miniText:{
+      fontSize: 16,
+      flex: 2
+    }
+  }
+});
 
 class ProfileAndSettings extends Component{
   static navigationOptions = {
   };
 
-  render(){
+  render(){ 
+    const style = styles.ProfileAndSettings;
     return(
-      <View>
-        <Text> Profile Link </Text>
-        <Text> Settings Link </Text>
+      <View style={style.container}>
+        <TouchableOpacity style={style.tap}>
+          <Image style={style.profile}source={require('../Assets/Profile_Pic.png')}/>
+          <Text style={style.miniText}> Profile </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={style.tap}>
+          <Text style={style.miniText}> Settings</Text>
+          <Image style={style.gear}source={require('../Assets/Gear.png')}/>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -108,9 +170,9 @@ class DrawerInformation extends Component{
 const SideMenu =  createDrawerNavigator({ //first object is the route configs
     Map //MapComponent TODO
   },{ //second object is the drawer navigator configs
-    drawerBackgroundColor: 'gray',
+    drawerBackgroundColor: 'whitesmoke',
     drawerType: 'slide',
-    drawerWidth: 250,
+    drawerWidth: .65*vw,
     contentComponent: DrawerInformation
   }
 );
