0801f3505ff12634881de20be6e9d49e309033e2
Sami <BlueBubbleBeam@outlook.com>
2019-10-26 11:20:22 -0400

    custom dashboard

--- MeetUp/App.js
+++ MeetUp/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, StatusBar} from 'react-native';
 import Login from './Components/Login';
 import Main from './Components/Main';
 //import Example from './Components/example';
--- MeetUp/Components/Dashboard.js
+++ MeetUp/Components/Dashboard.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import {createDrawerNavigator, DrawerNavigatorItems} from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
-import {TouchableOpacity, Button,Text, View, SafeAreaView, ScrollView, StyleSheet, Dimensions, Image } from 'react-native';
+import {TouchableOpacity, Button,Text, View, SafeAreaView, 
+  ScrollView, StyleSheet, Dimensions, Image, Animated, FlatList } from 'react-native';
 import Main from './Main'; 
-import { TextInput } from 'react-native-gesture-handler';
+import { Swipable } from 'react-native-gesture-handler/Swipeable';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 const vh = Dimensions.get("window").height;
 const vw = Dimensions.get("window").width;
@@ -26,7 +28,24 @@ class Greeting extends Component {    //each dashboard component gets its own na
     };
 
     render() {
-      const style = styles.Greeting;
+      const style = StyleSheet.create({
+        container:{
+          borderWidth: 2,
+          borderColor: '#F0F0F0',
+          marginLeft: .05*vw,
+          marginRight: .05*vw,
+          paddingTop: .05*vh,
+          paddingBottom: .05*vh,
+          borderStyle: 'dashed',
+          borderRadius: 1
+        },
+        text:{
+          fontSize: 24,
+          textAlign: 'center',
+          color: 'gold',
+        }
+      });
+
       return (
         <View style = {style.container}>
           <Text style={style.text}>Welcome Back,</Text>
@@ -36,64 +55,45 @@ class Greeting extends Component {    //each dashboard component gets its own na
     }
 }
 
-const styles = StyleSheet.create({
-  Greeting:{
-    container:{
-      borderWidth: 2,
-      borderColor: '#F0F0F0',
-      marginLeft: .05*vw,
-      marginRight: .05*vw,
-      paddingTop: .05*vh,
-      paddingBottom: .05*vh,
-      borderStyle: 'dashed',
-      borderRadius: 1
-    },
-    text:{
-      fontSize: 24,
-      textAlign: 'center',
-      color: 'gold',
-    }
-  },
-  ProfileAndSettings:{
-    container:{
-      paddingTop: .005*vh,
-      paddingBottom: .005*vh,
-      borderTopWidth: 2,
-      borderBottomWidth: 2,
-      borderColor: 'black',
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    },
-    tap:{
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      alignItems: 'center'
-    },
-    profile:{
-      height: 42,
-      width: 42,
-      flex: 1
-    },
-    gear:{
-      height: 38,
-      width: 38,
-      flex: 1
-    },
-    miniText:{
-      fontSize: 16,
-      flex: 2
-    }
-  }
-});
-
 class ProfileAndSettings extends Component{
   static navigationOptions = {
   };
 
   render(){ 
-    const style = styles.ProfileAndSettings;
+
+    const style = StyleSheet.create({
+      container:{
+        paddingTop: .005*vh,
+        paddingBottom: .005*vh,
+        borderTopWidth: 2,
+        borderBottomWidth: 2,
+        borderColor: 'black',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+      },
+      tap:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+      },
+      profile:{
+        height: 42,
+        width: 42,
+        flex: 1
+      },
+      gear:{
+        height: 38,
+        width: 38,
+        flex: 1
+      },
+      miniText:{
+        fontSize: 16,
+        flex: 2
+      }
+    });
+
     return(
       <View style={style.container}>
         <TouchableOpacity style={style.tap}>
@@ -109,16 +109,86 @@ class ProfileAndSettings extends Component{
   }
 }
 
-class FriendRequests extends Component{
-  static navigationOptions = {
-  };
 
+class FriendRequests extends Component{
+  constructor(props){
+    super(props);
+    this.state= {
+      requests: [{text: "You have no friend Request", id:0},{text: "You have one friend Request", id:1}],  //using a stack to keep track of all of the user's requests
+    }
+    this.ref =React.createRef();
+  }
+
+
+  //friend requests stack ontop of eachother
   render(){
+
+    const style = StyleSheet.create({
+      container:{
+        backgroundColor: '#88ffaa',
+        borderWidth: 2,
+        borderColor: '#9a6b3c',
+        marginTop: 20,
+        marginHorizontal: 20,
+        height: 45,
+        borderRadius: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      LeftActions:{
+        backgroundColor: '#38f477',
+        justifyContent: 'center',
+        flex: 1
+      },
+      text:{
+        color: '#000000',
+        fontWeight: '600',
+        fontSize: 18
+      },
+      accept: {
+        backgroundColor: '#aaFF22',
+        justifyContent: 'center',
+        flex: 1
+      },
+    });
+
+    const LeftActions = (progress, dragX) => {
+      const scale = dragX.interpolate({
+        inputRange : [0,100],
+        outputRange: [0,1],
+        extrapolate: 'clamp'
+      });
+
+      return (
+        <View style = {style.accept}>
+          {/* <Animated.Image style={[style.check, {transform: [{scale}]}]} source={require('../Assets/Check.png')}/>*/}
+          <Animated.Text style = {[style.text, {transform: [{scale}]}]}> Accept </Animated.Text>
+        </View> 
+      );
+    };
+      
     return(
-      <View> 
-        <Text>You have a new Friend request</Text>
-      </View>
-    );
+          <Swipeable
+            containerStyle= {style.container}
+            renderLeftActions={LeftActions}
+            onSwipeableLeftOpen={() => 
+              {
+                this.close;
+                let arr = this.state.requests;
+                let temp = arr.slice(0,arr.length-1);
+                this.setState({
+                  requests: temp
+                })
+              } 
+            }
+          > 
+            <View>
+              <Text style ={style.text}>
+                {this.state.requests[this.state.requests.length-1].text}
+              </Text>
+            </View>
+          </Swipeable>
+      );
   }
 }
 
@@ -172,7 +242,7 @@ const SideMenu =  createDrawerNavigator({ //first object is the route configs
   },{ //second object is the drawer navigator configs
     drawerBackgroundColor: 'whitesmoke',
     drawerType: 'slide',
-    drawerWidth: .65*vw,
+    drawerWidth: .70*vw,
     contentComponent: DrawerInformation
   }
 );
